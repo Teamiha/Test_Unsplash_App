1472fd882ef92d68c59dd8c6cb6549d33a8c309b
Mikhail Svetlov <jegnum@gmail.com>
2023-01-04 23:53:00 +0300

    Make project stuct

---
+++ Test_Unsplash_App/Application/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  Test_Unsplash_App
+//
+//  Created by Михаил Светлов on 04.01.2023.
+//
+
+import UIKit
+import CoreData
+
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.overrideUserInterfaceStyle = .light
+        runLaunchScreen()
+//        runMainFlow()
+        
+        return true
+    }
+    
+    
+//    func runMainFlow() {
+//        DispatchQueue.main.async {
+//            self.window?.rootViewController = TabBarConfigurator().configure()
+//        }
+//    }
+
+    func runLaunchScreen() {
+        let launchScreenViewController = UIStoryboard(name: "LaunchScreen", bundle: .main)
+            .instantiateInitialViewController()
+
+        window?.rootViewController = launchScreenViewController
+    }
+    
+    
+    
+//    func applicationWillTerminate(_ application: UIApplication) {
+//        StorageManager.shared.saveContext()
+//        }
+
+}
+
